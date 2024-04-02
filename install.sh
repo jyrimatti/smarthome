ad79c7826e04adccb4c7241ff623934a6b612244
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-04-02 23:00:54 +0300

    fix nginx install

--- install.sh
+++ install.sh
@@ -21,44 +21,12 @@ After=network-online.target remote-fs.target nss-lookup.target
 Wants=network-online.target
 
 [Service]
-Type=exec
-SupplementaryGroups=acme
-RuntimeDirectory=nginx
-LogsDirectory=nginx
-CacheDirectory=nginx
-
-ExecStart=/home/pi/.nix-profile/bin/nginx -c $configdir/nginx.conf
+Type=forking
+ExecStart=/home/pi/.nix-profile/bin/nginx -e stderr -c $configdir/nginx.conf
 ExecReload=/bin/kill -HUP $MAINPID
 
 Restart=on-failure
 RestartSec=10s
-
-# filesystem access
-ProtectSystem=strict
-ProtectHome=true
-PrivateTmp=true
-PrivateDevices=true
-ProtectControlGroups=true
-ProtectKernelModules=true
-ProtectKernelTunables=true
-
-# network
-RestrictAddressFamilies=AF_UNIX AF_INET AF_INET6
-
-# misc
-SystemCallArchitectures=native
-NoNewPrivileges=true
-RestrictRealtime=true
-MemoryDenyWriteExecute=true
-ProtectKernelLogs=true
-LockPersonality=true
-ProtectHostname=true
-RemoveIPC=true
-RestrictSUIDSGID=true
-ProtectClock=true
-
-# capabilities
-AmbientCapabilities=CAP_NET_BIND_SERVICE
 
 [Install]
 WantedBy=multi-user.target
